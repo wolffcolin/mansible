c35604ccce2b5849b9651a28a95884be4d811553
wolffcolin <99386239+wolffcolin@users.noreply.github.com>
2025-06-28 15:42:16 -0700

    shell layout setup

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ShellComponent } from './layout/shell/shell.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ShellComponent
+    }
+];
