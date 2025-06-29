49234330ed2250256e8a6470eef811722b2eab26
colin <cwcnsa@gmail.com>
2025-06-29 15:27:00 -0700

    half broken+

--- backend/src/main/java/com/example/mansible/Host.java
+++ backend/src/main/java/com/example/mansible/Host.java
@@ -1,1 +1,22 @@
-z   
+package com.example.mansible;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Host {
+
+    String address;
+
+    Map<String, String> vars = new HashMap<>();
+
+    @Override
+    public String toString() {
+        String inventoryLine = address + " ";
+        for (String key : vars.keySet()) {
+            inventoryLine = inventoryLine + key + "=" + vars.get(key) + " ";
+        }
+
+        return inventoryLine;
+    }
+
+}
