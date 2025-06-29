09e9c022784b81becc7d28e95fa867121ef8867b
wolffcolin <99386239+wolffcolin@users.noreply.github.com>
2025-06-28 17:06:04 -0700

    added sidenav

---
+++ frontend/src/app/components/devices/devices.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-devices',
+  imports: [],
+  templateUrl: './devices.component.html',
+  styleUrl: './devices.component.scss'
+})
+export class DevicesComponent {
+
+}
