68707209235a79b47b22fddfadfb4c3fa1cd456c
javds <avrgdeer@gmail.com>
2025-06-30 13:58:28 -0700

    small terminal window at the bottom working

--- frontend/src/app/layout/shell/shell.component.ts
+++ frontend/src/app/layout/shell/shell.component.ts
@@ -5,6 +5,7 @@ import { RouterLink } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbar, MatToolbarModule } from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-shell',
@@ -14,11 +15,22 @@ import { MatToolbar, MatToolbarModule } from '@angular/material/toolbar';
     MatToolbarModule,
     RouterOutlet,
     RouterLink,
-    MatIconModule
+    MatIconModule,
+    CommonModule
   ],
   templateUrl: './shell.component.html',
   styleUrl: './shell.component.scss'
 })
 export class ShellComponent {
+  drawerOpen = true;
 
+    commands = [
+    { text: 'ng build', status: 'success' },
+    { text: 'ng test', status: 'error' },
+    { text: 'ng lint', status: 'pending' }
+  ];
+
+  toggleDrawer() {
+    this.drawerOpen = !this.drawerOpen;
+  }
 }
